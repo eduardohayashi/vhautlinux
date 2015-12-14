4a322339edeaf2489b214de121f8032195fe7fb6
Eduardo Hayashi <eduardo.hayashi@climatempo.com.br>
2015-12-14 14:23:48 -0200

    PersonalizaÃao do script
    - Usando caminho completo para vhost
    - Inserindo email

--- geravhost.sh
+++ geravhost.sh
@@ -13,22 +13,28 @@
 ###########################################################
 #!/bin/bash
  
+echo "GerarVirtualHost v1.1"
+echo "**** REQUER SUDO ****"
+
 echo "Informe o nome do servidor (Ex.: siteexemplo) :"
 read vhost
  
-echo "Informe o caminho do site (Ex.: /var/www/sitexemplo) :"
+echo "Informe o caminho COMPLETO do site (Ex.: ~/workspace/sitexemplo) :"
 read path
+
+echo "Informe o email:"
+read email
  
 echo "Criando configuração de VHost para o servidor"
 
 #aqui é criado o arquivo de virtual host para o domínio e 
 #é feita a escrita das configurações no arquivo
 echo "<VirtualHost *:80>
-    	ServerAdmin webmaster@localhost
+    	ServerAdmin $email
     	ServerName $vhost
-    	ServerAlias www.$vhost 
-    	DocumentRoot /var/www/$path
-   	 <Directory /var/www/$path>
+    	ServerAlias $vhost 
+    	DocumentRoot $path
+   	 <Directory $path>
        		Options Indexes FollowSymLinks MultiViews
         	AllowOverride All
         	Order allow,deny
@@ -42,7 +48,7 @@ a2ensite $vhost.conf
 
 #escreve no arquivo de hosts do linux
 echo "Atualizando arquivo hosts"
-echo "127.0.1.1     $vhost www.$vhost" >> /etc/hosts
+echo "127.0.1.1     $vhost $vhost" >> /etc/hosts
 
 #renicia-se o servidor apache para aplicar as configurações
 echo "Reiniciando apache";
